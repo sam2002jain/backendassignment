2cee54196e707c1331c12cc8d255eea704a13ac8
Sanyam Jain <81524726+sam2002jain@users.noreply.github.com>
2024-12-12 20:20:29 +0530

    Update server.js 1.3.0

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb://localhost:27017/formbuilder', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
