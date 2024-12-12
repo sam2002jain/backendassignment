8a24368fa54b33e7699955e47d44252edad746be
Sanyam Jain <81524726+sam2002jain@users.noreply.github.com>
2024-12-12 20:57:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { Form, Response } = require('./models/Form');
+const { Form, Response } = require('./Form');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
