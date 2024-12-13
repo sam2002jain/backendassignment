1c7191bfa44a7dd735f7afe3d3240e43d67bdc75
Sanyam Jain <81524726+sam2002jain@users.noreply.github.com>
2024-12-13 13:49:37 +0530

    Update server.js 5.3.0

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Form = require('./form');
+const Form = require('./Form');
 
 dotenv.config();
 
